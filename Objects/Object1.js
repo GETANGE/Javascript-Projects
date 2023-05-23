f849c7e786b93a3633a4d4884f735d33e07424d7
Getange <emmanuelgetange48@gmail.com>
2023-05-23 08:34:30 +0300

    ADD:Object Implementation

---
+++ Objects/Object1.js
@@ -0,0 +1,59 @@
+let Emmanuel={
+    Name: "Name: Emmanuel Getange",
+    Occupation: "Occupation: Fullstack web developer",
+    BaseSalary: "Base salary: $150000",
+    Gender:"Gender: Male"
+}
+
+function emmanuel1(){
+    document.getElementById("info").innerHTML=Emmanuel.Name+"<br>"
+    +Emmanuel.Occupation+"<br>"+Emmanuel.BaseSalary+"<br>"+Emmanuel.Gender
+}
+
+let joy={
+    Name: "Name: JoyElizabeth Mothoni",
+    Occupation: "Occupation: Fullstack web developer",
+    BaseSalary: "Base salary: $150000",
+    Gender:"Gender: Female"
+}
+
+function joy1(){
+    document.getElementById("info").innerHTML=joy.Name+"<br>"
+    +joy.Occupation+"<br>"+joy.BaseSalary+"<br>"+joy.Gender
+}
+
+let Daniel={
+    Name: "Name: DanielBrian Gatuhu",
+    Occupation: "Occupation: Backend Android developer",
+    BaseSalary: "Base salary: $150000",
+    Gender:"Gender: Male"
+}
+
+function Daniel1(){
+    document.getElementById("info").innerHTML=Daniel.Name+"<br>"
+    +Daniel.Occupation+"<br>"+Daniel.BaseSalary+"<br>"+Daniel.Gender
+}
+
+let Emilio={
+    Name: "Name: Emilio Kariuki",
+    Occupation: "Occupation: Fullstack Flutter developer",
+    BaseSalary: "Base salary: $150000",
+    Gender:"Gender: Male"
+}
+
+function emilio1(){
+    document.getElementById("info").innerHTML=Emilio.Name+"<br>"
+    +Emilio.Occupation+"<br>"+Emilio.BaseSalary+"<br>"+Emilio.Gender
+}
+
+let Anthony={
+    Name: "Name: Anthony Momanyi",
+    Occupation: "Occupation: Android developer",
+    BaseSalary: "Base salary: $150000",
+    Gender:"Gender: Male"
+}
+
+function Anthony1(){
+    document.getElementById("info").innerHTML=Anthony.Name+"<br>"
+    +Anthony.Occupation+"<br>"+Anthony.BaseSalary+"<br>"+Anthony.Gender
+}
